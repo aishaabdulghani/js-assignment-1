b13c01e0d86914e531c4e51cb25f27cf6ce43757
Shahjahan M.Aslam <shahjahanmuhammadaslam@gmail.com>
2022-10-31 02:07:41 +0500

    changes

---
+++ app.js
@@ -0,0 +1,82 @@
+alert("chapter 1")
+window.alert("hello javascript")
+
+var chapter = "chapter 2"
+alert(chapter)
+
+var watching = "Thanks for watching "
+alert(watching)
+
+alert("chapter 3")
+
+var num ="155"
+alert(num)
+
+alert("chapter 4")
+var abc = "how are you"
+alert(abc)
+
+var _12ab = "I am fine"
+alert(_12ab)
+
+alert("chapter 5")
+var popularnumber = 4
+alert(popularnumber)
+
+
+alert(5 + 5)
+
+alert(12 - 24)
+
+var jet = 3 * 12
+alert(jet)
+
+var tit = 10
+alert(tit + 200)
+
+var kit = 50
+var lit = 40
+alert(kit + 40)
+
+var get ="chapter 6"
+alert(get)
+
+var a = 10;
+var b = 20;
+
+a++;
+
+console.log(a);
+
+var c = 40
+var d = 60
+
+++c
+
+alert(c)
+
+var fit = 55;
+var hit = fit++;
+
+console.log(hit);       
+
+alert("chapter 7")
+
+var rid = 1 + 3 * 4
+console.log(rid)
+
+var win = (2 * 4) * 4 + 2
+console.log(win)
+
+var him = (2 * 4) * 2 * 4
+console.log(him)
+
+alert("chapter 8")
+var lim = "message"
+var kim = "username"
+var jim = "banger"
+alert(lim + kim + jim)
+
+console.log("2" + "2")
+
+alert("8 * 8" + "8 * 8")
